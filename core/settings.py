503899c81533b4be4ef6b434af0027e726ca0b94
Yildum David Jonah <dyildum@gmail.com>
2025-08-24 13:20:05 +0100

    Fix STATIC_ROOT for collectstatic and restore base.html with favicon

--- core/settings.py
+++ core/settings.py
@@ -78,7 +78,9 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_URL = '/login/'  # Add this line
+LOGIN_URL = '/login/'
